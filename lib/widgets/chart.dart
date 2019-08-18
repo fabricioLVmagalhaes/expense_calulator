10f5333c266ce3849f81523eaf178fd5dd2c6c35
Fabricio Magalhães <fabricio.lvm@gmail.com>
2019-08-18 15:37:43 -0300

    Creating Bars for our Chart

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -19,18 +19,23 @@ class Chart extends StatelessWidget {
           totalSum += recentTransactions[i].amount;
         }
       }
+      print(DateFormat.E().format(weekDay));
+      print(totalSum);
 
-      return {'day': DateFormat.E(weekDay), 'amount': totalSum};
+      return {'day': DateFormat.E().format(weekDay).substring(0, 1), 'amount': totalSum};
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    print(groupedTransactionValues);
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Row(
-        children: <Widget>[],
+        children: groupedTransactionValues.map((data){
+          return Text('${data['day']} : ${data['amount']}');
+        }).toList(),
       ),
     );
   }
