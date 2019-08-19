542c38f0ae1b56951e4e55d3158f0aae4131b3c1
Fabricio Magalhães <fabricio.lvm@gmail.com>
2019-08-18 23:10:57 -0300

    Deleting Transactions & Using IconButtons

--- lib/main.dart
+++ lib/main.dart
@@ -81,6 +81,12 @@ class _MyHomePageState extends State<MyHomePage> {
         });
   }
 
+  void _deleteTransaction(String id){
+    setState(() {
+      _userTransactions.removeWhere((tx) => tx.id == id);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -100,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
             Chart(_recentTransactions),
-            TransactionList(_userTransactions)
+            TransactionList(_userTransactions, _deleteTransaction)
           ],
         ),
       ),
